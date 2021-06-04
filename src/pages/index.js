8aa09df41d755195c098cd08b627374e423cfff5
Jeffrey Tang <jt12@illinois.edu>
2021-06-03 23:42:06 -0500

    Change links to ul

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import { Link } from 'gatsby'
 import { StaticImage } from 'gatsby-plugin-image'
 import React from 'react'
 import { Helmet } from 'react-helmet'
@@ -5,11 +6,16 @@ import styled from 'styled-components'
 
 import FallingFruitLogo from '../images/falling_fruit_logo.png'
 
-const Links = styled.div`
-  a {
+const Links = styled.ul`
+  margin: 0;
+  padding: 0;
+
+  li {
+    display: inline;
+    margin-left: 0;
     margin-right: 20px;
 
-    &::before {
+    a::before {
       content: none !important;
     }
   }
@@ -58,7 +64,10 @@ const IndexPage = () => (
         This summer I'll be a quant trader intern at IMC Trading. Previously, I
         was at Citadel, Citadel Securities, and The Tuesday Company.
       </p>
-      <p>My hobbies include photography, making pizza, and tennis.</p>
+      <p>
+        My hobbies include <Link to="/photos">photography</Link>, making pizza,
+        and tennis.
+      </p>
       {/*
       <p>
         <strong>
@@ -84,10 +93,18 @@ const IndexPage = () => (
     </main>
     <footer>
       <Links>
-        <a href="/Jeffrey_Tang_Resume.pdf">Resume</a>
-        <a href="https://github.com/Qrtn">GitHub</a>
-        <a href="https://www.linkedin.com/in/jeffrey-tang/">LinkedIn</a>
-        <a href="mailto:jt@jeffreytang.org">Email</a>
+        <li>
+          <a href="/Jeffrey_Tang_Resume.pdf">Resume</a>
+        </li>
+        <li>
+          <a href="https://github.com/Qrtn">GitHub</a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/jeffrey-tang/">LinkedIn</a>
+        </li>
+        <li>
+          <a href="mailto:jt@jeffreytang.org">Email</a>
+        </li>
       </Links>
     </footer>
   </>

a09231bfba37c0b554c8095fa1ba2573070949b3
Jeffrey Tang <jt12@illinois.edu>
2021-06-04 02:12:52 -0500

    Switch to react-masonry-component

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,6 +1,6 @@
 import { GatsbyImage } from 'gatsby-plugin-image'
-import Masonry from 'masonry-layout'
-import React, { useEffect, useRef } from 'react'
+import React from 'react'
+import Masonry from 'react-masonry-component'
 import styled from 'styled-components/macro'
 
 const GUTTER = 10
@@ -11,30 +11,19 @@ const MASONRY_OPTIONS = {
   gutter: GUTTER,
 }
 
-const StyledGallery = styled.div`
-  a {
-    margin-bottom: ${GUTTER}px;
-    width: calc((100% - ${(COLUMNS - 1) * GUTTER}px) / ${COLUMNS});
-  }
+const GalleryItem = styled.a`
+  margin-bottom: ${GUTTER}px;
+  width: calc((100% - ${(COLUMNS - 1) * GUTTER}px) / ${COLUMNS});
 `
 
-const Gallery = ({ images }) => {
-  const gridRef = useRef()
-
-  useEffect(() => {
-    new Masonry(gridRef.current, MASONRY_OPTIONS)
-    console.log('new masonry')
-  }, [])
-
-  return (
-    <StyledGallery ref={gridRef}>
-      {images.map(({ id, gatsbyImageData }) => (
-        <a key={id} href={gatsbyImageData.images.fallback.src}>
-          <GatsbyImage key={id} image={gatsbyImageData} alt={id} />
-        </a>
-      ))}
-    </StyledGallery>
-  )
-}
+const Gallery = ({ images }) => (
+  <Masonry options={MASONRY_OPTIONS}>
+    {images.map(({ id, gatsbyImageData }) => (
+      <GalleryItem key={id} href={gatsbyImageData.images.fallback.src}>
+        <GatsbyImage key={id} image={gatsbyImageData} alt={id} />
+      </GalleryItem>
+    ))}
+  </Masonry>
+)
 
 export default Gallery

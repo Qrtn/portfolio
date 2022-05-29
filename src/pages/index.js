97f5220364a5028989019a6cbb7cc81801df5551
Jeffrey Tang <jeffrey.tang80@gmail.com>
2022-05-28 17:32:47 -0700

    Update bio

--- src/pages/index.js
+++ src/pages/index.js
@@ -49,11 +49,13 @@ const IndexPage = () => (
     </header>
     <main>
       <p>
-        Currently in my final year studying CS at UIUC, and interning at{' '}
-        <a href="https://ramp.com/">Ramp</a> as a software engineer.
+        Living in San Francisco and working as a founding engineer at Lassie.{' '}
+        <a href="https://lassie.notion.site/Founding-Business-Hire-5a637c7c2d064535830f9a8fc2f030a7">
+          We're hiring!
+        </a>
       </p>
       <p>
-        Previously, I was a tech lead at{' '}
+        Previously, I studied CS at UIUC. I was a tech lead at{' '}
         <a href="https://uiuc.hack4impact.org/">Hack4Impact</a>, where I built
         open-source software for non-profits. I worked with a team on{' '}
         <a href="https://falling-fruit.vercel.app/">Falling Fruit</a>
@@ -61,15 +63,15 @@ const IndexPage = () => (
         world.
       </p>
       <p>
-        In industry, I've also worked at{' '}
+        In industry, I've also worked at <a href="https://ramp.com/">Ramp</a>,{' '}
         <a href="https://www.imc.com">IMC Trading</a>,{' '}
         <a href="https://www.citadel.com/">Citadel</a> and{' '}
         <a href="https://www.citadelsecurities.com/">Citadel Securities</a>, and{' '}
         <a href="https://www.tuesdaycompany.com/">The Tuesday Company</a>.
       </p>
       <p>
-        My hobbies include <Link to="/photos">photography</Link>,{' '}
-        <Link to="/photos/pizza">making pizza</Link>, and tennis.
+        My hobbies include <Link to="/photos">photography</Link> and{' '}
+        <Link to="/photos/pizza">making pizza</Link>.
       </p>
     </main>
     <footer>
@@ -84,7 +86,12 @@ const IndexPage = () => (
           <a href="https://www.linkedin.com/in/jeffrey-tang/">LinkedIn</a>
         </li>
         <li>
-          <a href="mailto:jt@jeffreytang.org">Email</a>
+          <a
+            href="mailto:me@jeffreytang.com"
+            title="Please feel free to reach out!"
+          >
+            Email
+          </a>
         </li>
       </Links>
     </footer>
